bcb4f4e46e36e73eb75692af7ffd7f44011fdfc6
Sylvain <rama.sylvain@gmail.com>
2026-01-30 17:37:28 +0000

    Better planets

--- src/objs/planets.py
+++ src/objs/planets.py
@@ -1,68 +1,39 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from objs.common_schemas import Position, Velocity
+from common_schemas import MovableObject
+from dataclasses import dataclass
+from numpy.typing import NDArray
 
 
+@dataclass
 class Planet:
-    def __init__(self, radius: float, mu: float, atmosphere_height: float):
-        self.radius = radius
-        self.mu = mu
-        self.atmosphere_height = atmosphere_height
+    radius: float = 6371000.0  # m
+    mass: float = 5.972e24  # kg
+    spin_rate: float = 7.0882359e-5  # spin rate of plannet
+    G: float = 6.67408e-11  # gravitational constant, m^3/(kg*s^2)
 
-        if any([arg <= 0 for arg in [radius, mu, atmosphere_height]]):
-            raise ValueError("None of the arguments should be <= 0.")
+    def __post_init__(self):
+        # gravitational parameter, we ignore the mass of the secondary object
+        self.mu = self.G * self.mass
 
-    def gravity(self, pos: Position) -> tuple[float, float]:
-        r = pos.distance_to_core()
-        gx = -self.mu * pos.x / (r**3)
-        gy = -self.mu * pos.y / (r**3)
-        return gx, gy
+    def gravity(self, obj: MovableObject) -> NDArray[np.floating]:
+        r = obj.magnitude
+        g = -self.mu * obj.position / r**3
 
-    def atmosphere(self, pos: Position, vel: Velocity, drag_coeff: float) -> tuple[float, float]:
-        altitude = pos.altitude(self.radius)
-        rho = math.exp(-altitude / self.atmosphere_height)
-        v = math.hypot(vel.vx, vel.vy)
-        drag_x = -drag_coeff * rho * v * vel.vx
-        drag_y = -drag_coeff * rho * v * vel.vy
-        return drag_x, drag_y
+        return g
 
-    def show_characteristics(self, grid_n=100):
-
-        limit = (self.radius + self.atmosphere_height) * 2 + 10
-        xs = np.linspace(-limit, limit, grid_n)
-        ys = np.linspace(-limit, limit, grid_n)
-
-        grid_x, grid_y = np.meshgrid(xs, ys)
-
-        gravity_vals = []
-        atm_vals = []
-        for posx, posy in zip(np.ravel(grid_x), np.ravel(grid_y)):
-            pos = Position(float(posx), float(posy))
-            vel = Velocity(1, 1)
-            gravity_vals.append(self.gravity(pos))
-            atm_vals.append(self.atmosphere(pos, vel, drag_coeff=0.02))
-
-        gravity_vals = np.abs(np.reshape(gravity_vals, (grid_n, grid_n, 2)))
-        atm_vals = np.abs(np.reshape(atm_vals, (grid_n, grid_n, 2)))
-
-        fig, ax = plt.subplots(ncols=2, nrows=1, figsize=(10, 4))
-        ax = ax.flatten()
-
-        ax[0].imshow(np.sum(gravity_vals, axis=2), cmap="inferno")
-        ax[1].imshow(np.sum(atm_vals, axis=2), cmap="inferno")
-
-        planet = plt.Circle((grid_n // 2, grid_n // 2), self.radius, fill=False, color="r")
-        ax[0].add_artist(planet)
-        planet = plt.Circle((grid_n // 2, grid_n // 2), self.radius, fill=False, color="r")
-        ax[1].add_artist(planet)
-
-        return fig
+    def atmosphere(self, obj: MovableObject, drag_coeff: float) -> float:
+        altitude = obj.magnitude - self.radius
+        # rho = math.exp(-altitude / self.atmosphere_height)
+        # v = math.hypot(vel.vx, vel.vy)
+        # drag_x = -drag_coeff * rho * v * vel.vx
+        # drag_y = -drag_coeff * rho * v * vel.vy
+        return 0.0  # drag_x, drag_y
 
 
 if __name__ == "__main__":
-    planet = Planet(radius=100, mu=2.0e5, atmosphere_height=80)
-
-    fig = planet.show_characteristics()
-
-    plt.show()
+    earth = Planet()
+    pos_surface = MovableObject.from_list([0, earth.radius, 0], [0, 0, 0])
+    g = earth.gravity(pos_surface)
+    print(f"Gravity at surface: {g}")
