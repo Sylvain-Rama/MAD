24d9fa618215ee1f82215efc259cf438dc776f7f
Sylvain <rama.sylvain@gmail.com>
2026-01-31 15:02:00 +0000

    Better base object

--- src/objs/common_schemas.py
+++ src/objs/common_schemas.py
@@ -1,14 +1,17 @@
-from dataclasses import dataclass, field
-import math
 import numpy as np
 from numpy.typing import NDArray
+from constants import G
 
 
 class MovableObject:
-    def __init__(self, position: list[float], velocity: list[float] = [0.0, 0.0, 0.0]):
+    def __init__(self, position: list[float], velocity: list[float] | None = None, mass: float = 1.0):
 
-        self.position = np.asarray(position)
-        self.velocity = np.asarray(velocity)
+        self.position = np.asarray(position)  # m
+        if velocity:
+            self.velocity = np.asarray(velocity)  # m/s
+        else:
+            self.velocity = np.zeros_like(self.position)
+        self.mass = mass  # kg
         self.active: bool = True
 
     @property
@@ -17,51 +20,35 @@ class MovableObject:
 
     @property
     def norm(self) -> NDArray[np.floating]:
+        if self.magnitude == 0.0:
+            return np.zeros_like(self.position)
         return self.position / self.magnitude
+
+    def distance(self, other: "MovableObject"):
+        if not isinstance(other, type(self)):
+            raise ValueError(f"Distance can only be calculated between two MovableObject, got {type(other)}")
+
+        return np.sqrt(np.sum(np.square(self.position - other.position)))
+
+    def gravity(self, other: "MovableObject") -> float:
+        if not isinstance(other, ("MovableObject")):
+            raise ValueError(f"Gravity can only be calculated between two MovableObject, got {type(other)}")
+
+        dist = self.distance(other)
+        grav = G * self.mass * other.mass / dist**2
+        return grav
 
     def __repr__(self):
         a = "active" if self.active else "inactive"
         return f"Movable at {self.position}, velocity {self.velocity}, {a}."
 
 
-@dataclass
-class Position:
-    x: float
-    y: float
-
-    @classmethod
-    def from_angle_radius(cls, angle, radius):
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-
-        return cls(x, y)
-
-    def distance_to_core(self) -> float:
-        dist = math.hypot(self.x, self.y)
-        return 1e-10 if dist == 0 else dist
-
-    def altitude(self, radius: float) -> float:
-        r = self.distance_to_core()
-        return max(0.0, r - radius)
-
-    def __repr__(self) -> str:
-        return f"Position {self.x:4.2f}, {self.y:4.2f}"
-
-    def to_tuple(self) -> tuple[float, float]:
-        return (self.x, self.y)
-
-
-@dataclass
-class Velocity:
-    vx: float
-    vy: float
-
-    def __repr__(self) -> str:
-        return f"Velocity {self.vx:4.2f}, {self.vy:4.2f}"
-
-
 if __name__ == "__main__":
-    obj = MovableObject(position=np.array([3, 3, 0]), velocity=np.array([0, 0, 0]))
+    obj1 = MovableObject(position=[1, 1, 0])
+    obj2 = MovableObject(position=[1, 1, 1])
 
-    print(obj.magnitude)
-    print(obj.norm)
+    print(obj1)
+    print(obj1.magnitude)
+    print(obj1.norm)
+
+    print(obj1.distance(obj2))
