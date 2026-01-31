c2802a12f8685ae3b0559cf8551608a56cf20639
Sylvain <rama.sylvain@gmail.com>
2026-01-31 15:33:19 +0000

    Added constants

--- src/objs/constants.py
+++ src/objs/constants.py
@@ -1,1 +1,12 @@
+from dataclasses import dataclass
+
 G: float = 6.67408e-11  # m^3/(kg*s^2)
+
+
+EARTH_SETTINGS = {
+    "position": [0, 0, 0],  # center of the sim
+    "radius": 6371000.0,  # m
+    "mass": 5.972e24,  # kg
+    "spin_rate": 7.0882359e-5,  # rad/s
+    "name": "Earth",
+}
