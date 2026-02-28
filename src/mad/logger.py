75d19d83cf4ec0e66ac88cc804d5fcf1801fc091
Sylvain <rama.sylvain@gmail.com>
2026-02-28 19:17:51 +0000

    Added a proper logger

---
+++ src/mad/logger.py
@@ -0,0 +1,60 @@
+from loguru import logger
+import sys
+
+SOURCE_COLORS = {"Simulation": "<black>", "Missile": "<blue>", "Physics": "<green>"}
+
+
+def formatter(record):
+    source = record["extra"].get("source", "unknown")
+    color = SOURCE_COLORS.get(source, "<cyan>")
+
+    return "<green>{time:HH:mm:ss}</green> | " "<level>{level:<8}</level> | " f"{color}{source:<12}</> | " "{message}\n"
+
+
+def get_logger():
+    logger.remove()
+    logger.add(
+        sys.stdout,
+        format=formatter,
+        colorize=True,
+    )
+
+    return logger
+
+
+class _BoundSourceLogger:
+
+    def __init__(self, base_logger, source: str):
+        self._logger = base_logger.bind(source=source)
+
+    def debug(self, message, *a, **kw):
+        self._logger.debug(message, *a, **kw)
+
+    def info(self, message, *a, **kw):
+        self._logger.info(message, *a, **kw)
+
+    def warning(self, message, *a, **kw):
+        self._logger.warning(message, *a, **kw)
+
+    def error(self, message, *a, **kw):
+        self._logger.error(message, *a, **kw)
+
+    def critical(self, message, *a, **kw):
+        self._logger.critical(message, *a, **kw)
+
+
+class SourceLogger:
+
+    def __init__(self, base_logger):
+        self._logger = base_logger
+
+    def __getitem__(self, source: str):
+        return _BoundSourceLogger(self._logger, source)
+
+
+if __name__ == "__main__":
+
+    madlogger = SourceLogger(get_logger())
+    madlogger["Simulation"].warning("test")
+    madlogger["Missile"].info("info")
+    madlogger["dfdsf"].critical("critical")
