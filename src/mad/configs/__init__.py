919fb4ea89a1159a189e57fd96ea0cca4fc55249
Sylvain <rama.sylvain@gmail.com>
2026-05-13 22:04:06 +0000

    Update test notebook

---
+++ src/mad/configs/__init__.py
@@ -0,0 +1,14 @@
+from mad.configs.ballistic_objects import titan1_stages, titan2_stages, minuteman_stages
+from mad.configs.planets import EARTH_SETTINGS
+from mad.configs.satellites import sputnik
+from mad.configs.warheads import B53_warhead, rod_of_god
+
+__all__ = [
+    "titan1_stages",
+    "titan2_stages",
+    "minuteman_stages",
+    "EARTH_SETTINGS",
+    "sputnik",
+    "B53_warhead",
+    "rod_of_god",
+]
