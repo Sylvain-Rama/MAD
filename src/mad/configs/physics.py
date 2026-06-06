cce8afa13a7ff47496b23bf5cde718f5553b3901
Sylvain <rama.sylvain@gmail.com>
2026-06-06 10:15:11 +0000

    Corrected mach number, added Aster 30

--- src/mad/configs/physics.py
+++ src/mad/configs/physics.py
@@ -4,4 +4,4 @@ G: float = 6.67408e-11  # m^3/(kg*s^2)
 G0: float = 9.80665  # m/s^2, value for earth
 VOXEL_SIZE: float = 50_000.0  # m, size of voxels for broad-phase collision detection and radar detection
 POUNDS_FORCE_TO_NEWTONS: float = 4.44822  # N/lbf
-METERS_S_TO_MACH: float = 340.29  # m/s, speed of sound at sea level
+MACHS_TO_METERS_S: float = 340.29  # m/s, speed of sound at sea level
