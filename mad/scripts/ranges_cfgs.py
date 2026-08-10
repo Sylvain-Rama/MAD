0ee983b335ffca0f60e9628cc584a9892736caef
Sylvain <rama.sylvain@gmail.com>
2026-08-10 19:24:50 +0000

    Updated 04 - ballistic tables

--- mad/scripts/ranges_cfgs.py
+++ mad/scripts/ranges_cfgs.py
@@ -29,13 +29,20 @@ SIM_PARAMETERS: dict[str, dict[str, Any]] = {
         "dt": 10.0,
         "max_time": 3600.0,
     },
-    "B53_warhead": {
+    "B53_warhead_high": {
         "altitudes_km": np.arange(120, 180, 2),
         "velocities_kms": np.arange(2, 3, 0.1),
         "gammas_deg": np.arange(20, 90, 0.5),
         "dt": 2.0,
         "max_time": 3600.0,
     },
+    "B53_warhead_demo": {
+            "altitudes_km": np.arange(5, 500, 100),
+            "velocities_kms": np.arange(2, 3, 0.1),
+            "gammas_deg": np.arange(20, 90, 5.0),
+            "dt": 2.0,
+            "max_time": 3600.0,
+        },
     "V1": {
         "altitudes_km": np.arange(0.1, 1, 0.05),
         "velocities_kms": np.arange(0.1, 0.2, 0.01),
