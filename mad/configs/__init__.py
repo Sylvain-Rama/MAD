9f487f176ac7dc1ec94f0fabb1f02e62f369e2aa
Sylvain <rama.sylvain@gmail.com>
2026-08-28 13:16:03 +0000

    Moon mission notebook

--- mad/configs/__init__.py
+++ mad/configs/__init__.py
@@ -1,5 +1,5 @@
 from mad.configs.ballistic_objects_cfg import titan1_stages, titan2_stages, minuteman_stages, sputnik_stages
-from mad.configs.planets_cfg import EARTH_SETTINGS
+from mad.configs.planets_cfg import EARTH_SETTINGS, MOON_SETTINGS
 from mad.configs.satellites_cfg import sputnik
 from mad.configs.warheads_cfg import B53_warhead, rod_of_god
 from mad.configs.cruise_missiles_cfg import tomahawk, aster_30, tomahawk_guidance, aster_30_guidance, V1, V1_guidance
@@ -11,6 +11,7 @@ __all__ = [
     "titan2_stages",
     "minuteman_stages",
     "EARTH_SETTINGS",
+    "MOON_SETTINGS",
     "sputnik_stages",
     "sputnik",
     "B53_warhead",
